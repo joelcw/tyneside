7d2b34646aee79756370b625cc0c0e8132e230d2
Josef Fruehwald <JoFrhwld@gmail.com>
2013-04-04 10:14:04 -0400

    changed generate_new_speaker

--- articles/sim_Joel.R
+++ articles/sim_Joel.R
@@ -4,55 +4,55 @@ library(reshape2)
 library(plyr)
 
 
-generate_new_speaker <- function(niter, generation, min = 0, max = 100){
+generate_new_speaker <- function(niter, generation, min = 0, max = 100, old_speaker){
   # This function generates a new learner.
-#"In" and "Ing" are the (mean) style values for each variant
-#token is whihc variant they just heard
-#style is which style "
+  #"In" and "Ing" are the (mean) style values for each variant
+  #token is whihc variant they just heard
+  #style is which style "
   experience <- list(In    = vector(length = niter),
                      Ing   = vector(length = niter),
                      token = vector(length = niter),
                      style = vector(length = niter),
                      iter  = 1:niter,
                      generation = rep(generation, niter)
-                    )
-#  experience[["Ing"]][1]   <- (max-min)/2
-#  experience[["In"]][1]    <- (max-min)/2
-
-#JCW: I am changing the above to be values generated from old_speaker
-
-
-currentobs <- generate_from_speaker(old_speaker)
-
-if(currentobs[["token"]] == "Ing"){
-	while(currentobs[["token"]] == "Ing"){
-		lastobs <- currentobs
-		currentobs <- generate_from_speaker(old_speaker)
-	}
-
-   	experience[["Ing"]][1]   <- lastobs[["style"]]
-   	experience[["In"]][1]    <- currentobs[["style"]]
-	
-	#debug
-	print("Ing")
-	print(lastobs[["style"]])
-	print(currentobs[["style"]])
-}
-
-else {
-	while(currentobs[["token"]] == "In"){
-		lastobs <- currentobs
-		currentobs <- generate_from_speaker(old_speaker)
-	}
-
-   	experience[["Ing"]][1]   <- currentobs[["style"]]
-   	experience[["In"]][1]    <- lastobs[["style"]]
-	
-	#debug
-	print("In")
-	print(lastobs[["style"]])
-	print(currentobs[["style"]])
-}
+  )
+  #  experience[["Ing"]][1]   <- (max-min)/2
+  #  experience[["In"]][1]    <- (max-min)/2
+  
+  #JCW: I am changing the above to be values generated from old_speaker
+  
+  
+  currentobs <- generate_from_speaker(old_speaker)
+  
+  if(currentobs[["token"]] == "Ing"){
+    while(currentobs[["token"]] == "Ing"){
+      lastobs <- currentobs
+      currentobs <- generate_from_speaker(old_speaker)
+    }
+    
+    experience[["Ing"]][1]   <- lastobs[["style"]]
+    experience[["In"]][1]    <- currentobs[["style"]]
+    
+    #debug
+    print("Ing")
+    print(lastobs[["style"]])
+    print(currentobs[["style"]])
+  }
+  
+  else {
+    while(currentobs[["token"]] == "In"){
+      lastobs <- currentobs
+      currentobs <- generate_from_speaker(old_speaker)
+    }
+    
+    experience[["Ing"]][1]   <- currentobs[["style"]]
+    experience[["In"]][1]    <- lastobs[["style"]]
+    
+    #debug
+    print("In")
+    print(lastobs[["style"]])
+    print(currentobs[["style"]])
+  }
   experience[["token"]][1] <- NA
   experience[["style"]][1] <- NA
   return(experience)
@@ -60,27 +60,27 @@ else {
 
 update_speaker <- function(speaker, lambda, iter, token, style){
   # this function updates a learner with input
-#speaker is basically experience  
-other_token <- c(In = "Ing", Ing = "In")
+  #speaker is basically experience  
+  other_token <- c(In = "Ing", Ing = "In")
   other <- other_token[token]
-
-#expectation is a style value    
+  
+  #expectation is a style value    
   previous_expectation <- speaker[[token]][iter-1]
-
-#If its closer than 1 unit to the expectation than dist = 1
+  
+  #If its closer than 1 unit to the expectation than dist = 1
   dist    <- max(1, abs(style-previous_expectation))
-
-#weight is smaller at larger distances from the expectation
+  
+  #weight is smaller at larger distances from the expectation
   weight  <- lambda * (1/(dist^2))
-
-#check this update with Joe  
+  
+  #check this update with Joe  
   new_expectation <- (previous_expectation * (1-weight)) + (style * weight)
   
   speaker[[token]][iter]   <- new_expectation
   speaker[[other]][iter]   <- speaker[[other]][iter-1]
   speaker[["style"]][iter] <- style
   speaker[["token"]][iter] <- token
-    
+  
   return(speaker)
 }
 
@@ -88,12 +88,12 @@ generate_from_speaker <- function(speaker, min = 0, max = 100, eps = 0.001){
   # this function generates a tokens from a mature speaker
   #Choose style to speak, which is an independent choice
   style <- runif(1, min, max)
-
-#Takes their last expectation for the style of each token
+  
+  #Takes their last expectation for the style of each token
   In_expect  <- rev(speaker[["In"]])[1]
   Ing_expect <- rev(speaker[["Ing"]])[1]
   
-#eps is epsilon, the smallest value that something can take, as in clustering algorithms
+  #eps is epsilon, the smallest value that something can take, as in clustering algorithms
   In_dist  <- max(eps, abs(style - In_expect))
   Ing_dist <- max(eps, abs(style - Ing_expect))
   
@@ -112,9 +112,9 @@ generate_from_speaker <- function(speaker, min = 0, max = 100, eps = 0.001){
 
 #old_speaker should have SAME STYLE for in/ing, to model first generation after doublet is innovated
 old_speaker <- list(In    = 50,
-                     Ing   = 50,
-                     token = NA,
-                     style = NA)
+                    Ing   = 50,
+                    token = NA,
+                    style = NA)
 
 ngenerations <- 20
 niter <- 1000
@@ -124,10 +124,10 @@ generations <- vector(length = ngenerations, mode = "list")
 
 
 for(gen in 1:ngenerations){
-  new_speaker <- generate_new_speaker(niter = niter, generation=gen)
+  new_speaker <- generate_new_speaker(niter = niter, generation=gen, old_speaker = old_speaker)
   for(iter in 2:niter){
     obs <- generate_from_speaker(old_speaker)
-#obs is a token and its associated style    
+    #obs is a token and its associated style    
     token <- obs[["token"]]
     style <- obs[["style"]]
     
@@ -145,9 +145,9 @@ generations.df.melt <- melt(generations.df, value.name = "Estimate", id.vars = c
 pdf("1000iterPerGen.pdf")
 
 ggplot(generations.df.melt, aes(x = iter, y = Estimate, color=variant, group=variant))+
-    geom_step()+
-    facet_wrap(~generation)+
-    ylim(0,100)
+  geom_step()+
+  facet_wrap(~generation)+
+  ylim(0,100)
 
 #ggplot(generations.df, aes(iter, Ing))+
 #  geom_step()+
@@ -165,9 +165,9 @@ dev.off()
 
 #old_speaker should have SAME STYLE for in/ing, to model first generation after doublet is innovated
 old_speaker <- list(In    = 50,
-                     Ing   = 50,
-                     token = NA,
-                     style = NA)
+                    Ing   = 50,
+                    token = NA,
+                    style = NA)
 
 ngenerations <- 20
 niter <- 20000
@@ -177,10 +177,10 @@ generations <- vector(length = ngenerations, mode = "list")
 
 
 for(gen in 1:ngenerations){
-  new_speaker <- generate_new_speaker(niter = niter, generation=gen)
+  new_speaker <- generate_new_speaker(niter = niter, generation=gen, old_speaker = old_speaker)
   for(iter in 2:niter){
     obs <- generate_from_speaker(old_speaker)
-#obs is a token and its associated style    
+    #obs is a token and its associated style    
     token <- obs[["token"]]
     style <- obs[["style"]]
     
@@ -198,9 +198,9 @@ generations.df.melt <- melt(generations.df, value.name = "Estimate", id.vars = c
 pdf("20000iterPerGen.pdf")
 
 ggplot(generations.df.melt, aes(x = iter, y = Estimate, color=variant, group=variant))+
-    geom_step()+
-    facet_wrap(~generation)+
-    ylim(0,100)
+  geom_step()+
+  facet_wrap(~generation)+
+  ylim(0,100)
 
 #ggplot(generations.df, aes(iter, Ing))+
 #  geom_step()+
