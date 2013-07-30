9f0548edd0a10d494e1b7f506ed140c3edd7c5a0
joelcw <joel.wallenberg@gmail.com>
2013-07-30 19:31:15 -0400

    added portuguese and ns to script

--- extraposition/plotsandstats/extraposition_diach_plotsonly.R
+++ extraposition/plotsandstats/extraposition_diach_plotsonly.R
@@ -65,6 +65,9 @@ plot.data
 p <- ggplot(plot.data, aes(Time2, whet, color=Position, group=Position)) + labs(y = "Probability of Extraposition", x = "\nTime Period") + geom_point(aes(size = n)) + scale_size_area(max_size=12) + stat_smooth() + scale_alpha_continuous(guide="none", limits = c(0,.7)) + scale_color_brewer(palette = "Set1") + ylim(0,1)
 
 "finished plot"
+"N for English =" 
+sum(plot.data$n)
+
 
 ggsave(p, file = "exSbjObjYearBinned50Loess.ymeb.pdf", width = 8, height = 5)
 
@@ -136,6 +139,9 @@ plot.data <- ddply(ex.data, .(Time2,Position),summarize, whet = mean(Extraposed,
 
 
 p <- ggplot(plot.data, aes(Time2, whet, color=Position, group=Position)) + labs(y = "Probability of Extraposition", x = "\nTime Period") + geom_point(aes(size = n)) + scale_size_area(max_size=12) + stat_smooth() + scale_alpha_continuous(guide="none", limits = c(0,.7)) + scale_color_brewer(palette = "Set1") + ylim(0,1)
+
+"N for Icelandic =" 
+sum(plot.data$n)
 
 ggsave(p, file = "exSbjObjYearBinned50Loess.ice.pdf", width = 8, height = 5)
 
@@ -225,6 +231,80 @@ plot.data <- ddply(ex.data, .(Time2, Position),summarize, whet = mean(Extraposed
 plot.data
 
 p <- ggplot(plot.data, aes(Time2, whet, color=Position, group=Position)) + labs(y = "Probability of Extraposition", x = "\nTime Period") + geom_point(aes(size = n)) + scale_size_area(max_size=12) + stat_smooth() + scale_alpha_continuous(guide="none", limits = c(0,.7)) + scale_color_brewer(palette = "Set1") + ylim(0,1)
+
+"N for French =" 
+sum(plot.data$n)
+
+ggsave(p, file = "exSbjObjYearBinned50Loess.fre.pdf", width = 8, height = 5)
+
+
+####Plotting average weight of ex for subject only
+
+ex.data <- subset(ex.data, Position == "sbj")
+
+ex.data <- droplevels(ex.data)
+
+####Creates data frame with average weight for both extraposed and non extraposed, and n for each time period
+
+plot.data <- ddply(ex.data, .(Time2,Extraposed),summarize, whet = mean(Weight, na.rm = T), n = sum(!is.na(Extraposed)))
+
+"finished plot.data"
+
+plot.data$Extraposed <- as.character(plot.data$Extraposed)
+plot.data
+
+p <- ggplot(plot.data, aes(Time2, whet, color=Extraposed, group=Extraposed)) + labs(y = "Average Weight of Extraposed from Sbj Clauses", x = "\nTime Period") + geom_point(aes(size = n)) + scale_size_area(max_size=12) + stat_smooth() + scale_alpha_continuous(guide="none", limits = c(0,.7)) + scale_color_brewer(palette = "Set2")
+
+ggsave(p, file = "exWeightYearBinned50Loess.fre.pdf", width = 8, height = 5)
+
+####Portuguese
+####Read the file of CorpusSearch codes into an R data frame.
+
+foo <- read.delim("../queriesandoutput/cprelExtrapos.port.cod.ooo",header=F,sep=":")
+
+
+####Give appropriate column names to the columns
+
+colnames(foo) <- c("Extraposed","Position","Clause","TextOrSpeech", "Weight","Year","Text")
+
+
+####Throw out all the codes that refer to tokens that are irrelevant for the study.
+
+"Got up to subsetting"
+
+ex.data <- subset(foo,Extraposed != "z" & Clause != "z" & Position != "z" & Year != "z" & Year != "0" & Year != "" & Weight != "z")
+
+
+####Make sure R factor groups don't include factors for the irrelevant codes.
+
+ex.data <- droplevels(ex.data)
+
+"finished droplevels"
+
+####Make sure dates abd 0/1 codes are stored as numbers, and weights
+
+ex.data$Year <- as.numeric(as.character(ex.data$Year))
+ex.data$Extraposed <- as.numeric(as.character(ex.data$Extraposed))
+ex.data$Weight <- as.numeric(as.character(ex.data$Weight))
+
+"finished converting to numeric"
+
+
+
+####Create a new variable in the data frame which will bin the data by century. The only point to doing this is to make a pretty graph of percentages across time.
+
+ex.data$Time2 <- floor(ex.data$Year/50)*50
+
+####Create a new data frame for the percentage plot which averages percentages for each century. Again, this is not useful for real data analysis -- just for making this particular plot.
+
+plot.data <- ddply(ex.data, .(Time2, Position),summarize, whet = mean(Extraposed, na.rm = T), n = sum(!is.na(Extraposed)))
+
+plot.data
+
+p <- ggplot(plot.data, aes(Time2, whet, color=Position, group=Position)) + labs(y = "Probability of Extraposition", x = "\nTime Period") + geom_point(aes(size = n)) + scale_size_area(max_size=12) + stat_smooth() + scale_alpha_continuous(guide="none", limits = c(0,.7)) + scale_color_brewer(palette = "Set1") + ylim(0,1)
+
+"N for Portuguese =" 
+sum(plot.data$n)
 
 ggsave(p, file = "exSbjObjYearBinned50Loess.fre.pdf", width = 8, height = 5)
 
