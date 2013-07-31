68d925166d145d9899852c11cbe4c4ea7f5c0d44
joelcw <joel.wallenberg@gmail.com>
2013-07-31 12:35:29 -0400

    changed normal plot

--- extraposition/plotsandstats/normal.R
+++ extraposition/plotsandstats/normal.R
@@ -1,1 +1,3 @@
-ggplot(data.frame(x = c(0, 100)), aes(x)) + stat_function(fun = dnorm, args = list(mean = 25, sd = 10))+stat_function(fun = dnorm, args = list(mean = 75, sd = 10))
+p <- ggplot(data.frame(x = c(0, 100)), aes(x)) + stat_function(fun = dnorm, args = list(mean = 12.5, sd = 10))+stat_function(fun = dnorm, args = list(mean = 35.5, sd = 10))
+
+ggsave(p, file = "twonormals.pdf", width = 8, height = 5)
