6a52889386fb208d70507c96847ea3e554a7703a
joelcw <joel.wallenberg@gmail.com>
2016-05-09 17:29:45 +0100

    mdified for text ids

--- extraposition/plotsandstats/combineData.py
+++ extraposition/plotsandstats/combineData.py
@@ -1,8 +1,10 @@
+#this takes the extracted code CS output files and combines them, adding a label per language. This version is for the "fixed" versions, which have text ids added, for mixed effects modelling.
+
 import sys,string
 
 outfile = open("allLangsEx.cod.ooo","w")
 
-infile = open("../queriesandoutput/cprelExtrapos.ymeb.cod.ooo","r")
+infile = open("../queriesandoutput/cprelExtrapos.ymeb.cod.fixed.ooo","r")
 
 lines = infile.readlines()
 
@@ -10,7 +12,7 @@ for line in lines:
     line = line.replace("\n","")
     outfile.write("%s:English\n" % line)
 
-infile = open("../queriesandoutput/cprelExtrapos.ice.cod.ooo","r")
+infile = open("../queriesandoutput/cprelExtrapos.ice.cod.fixed.ooo","r")
 
 lines = infile.readlines()
 
@@ -20,7 +22,7 @@ for line in lines:
     if len(fields) >= 6:
         outfile.write("%s:%s:%s:%s:%s:%s:Icelandic\n" % (fields[0],fields[1],fields[2],fields[3],fields[4],fields[5]))
 
-infile = open("../queriesandoutput/cprelExtrapos.fre.cod.ooo","r")
+infile = open("../queriesandoutput/cprelExtrapos.fre.cod.fixed.ooo","r")
 
 lines = infile.readlines()
 
@@ -30,7 +32,7 @@ for line in lines:
     if len(fields) >=6:
         outfile.write("%s:%s:%s:%s:%s:%s:French\n" % (fields[0],fields[1],fields[2],fields[3],fields[4],fields[5]))
 
-infile = open("../queriesandoutput/cprelExtrapos.port.cod.ooo","r")
+infile = open("../queriesandoutput/cprelExtrapos.port.cod.fixed.ooo","r")
 
 lines = infile.readlines()
 
