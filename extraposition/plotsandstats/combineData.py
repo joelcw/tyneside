e2d34596d06b1d5e6e5c899ef396f5fccd8d431b
joelcw <joel.wallenberg@gmail.com>
2016-05-09 18:22:23 +0100

    fixed id things

--- extraposition/plotsandstats/combineData.py
+++ extraposition/plotsandstats/combineData.py
@@ -19,8 +19,11 @@ lines = infile.readlines()
 for line in lines:
     line = line.replace("\n","")
     fields = line.split(":")
-    if len(fields) >= 6:
-        outfile.write("%s:%s:%s:%s:%s:%s:Icelandic\n" % (fields[0],fields[1],fields[2],fields[3],fields[4],fields[5]))
+    if len(fields) >= 7:
+        outfile.write("%s:%s:%s:%s:%s:%s:%s:Icelandic\n" % (fields[0],fields[1],fields[2],fields[3],fields[4],fields[5],fields[6]))
+#debug
+#    else:
+ #       sys.stdout.write(line)
 
 infile = open("../queriesandoutput/cprelExtrapos.fre.cod.fixed.ooo","r")
 
@@ -29,8 +32,8 @@ lines = infile.readlines()
 for line in lines:
     line = line.replace("\n","")
     fields = line.split(":")
-    if len(fields) >=6:
-        outfile.write("%s:%s:%s:%s:%s:%s:French\n" % (fields[0],fields[1],fields[2],fields[3],fields[4],fields[5]))
+    if len(fields) >=7:
+        outfile.write("%s:%s:%s:%s:%s:%s:%s:French\n" % (fields[0],fields[1],fields[2],fields[3],fields[4],fields[5],fields[6]))
 
 infile = open("../queriesandoutput/cprelExtrapos.port.cod.fixed.ooo","r")
 
